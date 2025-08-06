5f9fa702cf3967e924cb1d6f237405ba5a755d85
Trịnh Hg <onepiecechapter@gmail.com>
2025-08-06 19:58:33 +0700

    Create style.js

---
+++ style.js
@@ -0,0 +1,16 @@
+/* style.css */
+.tab-btn {
+  transition: background-color 0.3s;
+}
+
+.tab-content {
+  transition: opacity 0.3s;
+}
+
+#previewContent, #chapterList {
+  min-height: 200px;
+}
+
+textarea {
+  resize: vertical;
+}
